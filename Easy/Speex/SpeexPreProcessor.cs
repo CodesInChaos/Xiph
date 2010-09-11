31cb6004265a754aa26ceb64ce812b63ce317ddd
CodeInChaos <CodeInChaos@nowhere>
2010-09-11 15:08:04 +0200

    Speex Pre Processor

--- Easy/Speex/SpeexPreProcessor.cs
+++ Easy/Speex/SpeexPreProcessor.cs
@@ -2,35 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using native = Xiph.LowLevel.SpeexPreProcessorNative;
+using Xiph.LowLevel;
 namespace Xiph.Easy.Speex
 {
-	/*public class SpeexPreProcessor : IDisposable
+	public class SpeexPreProcessor : IDisposable
 	{
-		public IntPtr SpeexPreprocessState { get; private set; }
-
-		public void SetControl(int request, int value)
-		{
-		}
-
-		public 
-
+		internal IntPtr State { get; private set; }
 		public int SamplesPerFrame { get; private set; }
 		public int SamplesPerSecond { get; private set; }
-		public bool DeNoise
+		public bool? VoiceActive { get; private set; }
+
+		private void SetControl(SpeexPreProcessorRequests request, int value)
 		{
-			get
+			if ((int)request % 2 == 1)
+				throw new ArgumentException("request is not a setter");
+			unsafe
 			{
-			}
-			set
-			{
+				if (native.speex_preprocess_ctl(State, (int)request, &value) != 0)
+					throw new InvalidOperationException("speex_preprocess_ctl returned error");
 			}
 		}
 
-		public SpeexPreProcessor(int samplesPerFrame, int samplesPerSecond)
+		private int GetControl(SpeexPreProcessorRequests request)
 		{
-			SamplesPerFrrame = samplesPerFrame;
+			if ((int)request % 2 == 0)
+				throw new ArgumentException("request is not a getter");
+			unsafe
+			{
+				int value = 0;
+				if (native.speex_preprocess_ctl(State, (int)request, &value) != 0)
+					throw new InvalidOperationException("speex_preprocess_ctl returned error");
+				return value;
+			}
+		}
+
+		public SpeexPreProcessor(int samplesPerSecond, int samplesPerFrame)
+		{
+			if (samplesPerFrame <= 0)
+				throw new ArgumentException("samplesPerFrame must be >0");
+			if (samplesPerSecond <= 0)
+				throw new ArgumentException("samplesPerSecond must be >0");
+			SamplesPerFrame = samplesPerFrame;
 			SamplesPerSecond = samplesPerSecond;
+			State = native.speex_preprocess_state_init(samplesPerFrame, samplesPerSecond);
 		}
 
 
@@ -41,7 +56,79 @@ namespace Xiph.Easy.Speex
 
 		public void Close()
 		{
-			SpeexPreprocessState = IntPtr.Zero;
+			if (State != IntPtr.Zero)
+			{
+				native.speex_preprocess_state_destroy(State);
+				State = IntPtr.Zero;
+			}
 		}
-	}*/
+
+		public void Run(Int16[] data, int offset)
+		{
+			Update(data, offset, false);
+		}
+
+		public void UpdateEstimate(Int16[] data, int offset)
+		{
+			Update(data, offset, true);
+		}
+
+		public void Update(Int16[] data, int offset, bool estimate)
+		{
+			if (offset < 0)
+				throw new ArgumentException("offset<0");
+			if (offset + SamplesPerFrame > data.Length)
+				throw new ArgumentException("data too small");
+			VoiceActive = null;
+			int result = 0;
+			unsafe
+			{
+				fixed (Int16* dataPtr = &(data[offset]))
+				{
+					if (!estimate)
+						result = native.speex_preprocess_run(State, dataPtr);
+					else
+						native.speex_preprocess_estimate_update(State, dataPtr);
+				}
+			}
+			if (VoiceActivityDetection && !estimate)
+				VoiceActive = result != 0;
+		}
+
+		public bool DeNoise
+		{
+			get
+			{
+				return GetControl(SpeexPreProcessorRequests.GET_DENOISE) != 0;
+			}
+			set
+			{
+				SetControl(SpeexPreProcessorRequests.SET_DENOISE, Convert.ToInt32(value));
+			}
+		}
+
+		public bool VoiceActivityDetection
+		{
+			get
+			{
+				return GetControl(SpeexPreProcessorRequests.GET_VAD) != 0;
+			}
+			set
+			{
+				SetControl(SpeexPreProcessorRequests.SET_VAD, Convert.ToInt32(value));
+			}
+		}
+
+		public bool AutomaticGainControl
+		{
+			get
+			{
+				return GetControl(SpeexPreProcessorRequests.GET_AGC) != 0;
+			}
+			set
+			{
+				SetControl(SpeexPreProcessorRequests.SET_AGC, Convert.ToInt32(value));
+			}
+		}
+	}
 }
---
+++ LowLevel/SpeexPreProcessor.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Xiph.LowLevel
+{
+	public enum SpeexPreProcessorRequests
+	{
+		SET_DENOISE = 0,
+		GET_DENOISE = 1,
+		SET_AGC = 2,
+		GET_AGC = 3,
+		SET_VAD = 4,
+		GET_VAD = 5,
+		SET_AGC_LEVEL = 6,
+		GET_AGC_LEVEL = 7,
+		SET_DEREVERB = 8,
+		GET_DEREVERB = 9,
+		SET_DEREVERB_LEVEL = 10,
+		GET_DEREVERB_LEVEL = 11,
+		SET_DEREVERB_DECAY = 12,
+		GET_DEREVERB_DECAY = 13,
+		SET_PROB_START = 14,
+		GET_PROB_START = 15,
+		SET_PROB_CONTINUE = 16,
+		GET_PROB_CONTINUE = 17,
+		SET_NOISE_SUPPRESS = 18,
+		GET_NOISE_SUPPRESS = 19,
+		SET_ECHO_SUPPRESS = 20,
+		GET_ECHO_SUPPRESS = 21,
+		SET_ECHO_SUPPRESS_ACTIVE = 22,
+		GET_ECHO_SUPPRESS_ACTIVE = 23,
+		SET_ECHO_STATE = 24,
+		GET_ECHO_STATE = 25,
+		SET_AGC_INCREMENT = 26,
+		GET_AGC_INCREMENT = 27,
+		SET_AGC_DECREMENT = 28,
+		GET_AGC_DECREMENT = 29,
+		SET_AGC_MAX_GAIN = 30,
+		GET_AGC_MAX_GAIN = 31
+	}
+
+	public static unsafe class SpeexPreProcessorNative
+	{
+		[DllImport("libspeexdsp", CallingConvention = CallingConvention.Cdecl)]
+		public static extern IntPtr speex_preprocess_state_init(int frame_size, int sampling_rate);
+		[DllImport("libspeexdsp", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void speex_preprocess_state_destroy(IntPtr st);
+		[DllImport("libspeexdsp", CallingConvention = CallingConvention.Cdecl)]
+		public static extern int speex_preprocess_run(IntPtr st, Int16* x);
+		/// <summary>
+		/// Preprocess a frame (deprecated, use speex_preprocess_run() instead)
+		/// </summary>
+		[DllImport("libspeexdsp", CallingConvention = CallingConvention.Cdecl)]
+		public static extern int speex_preprocess(IntPtr st, Int16* x, Int16* echo);
+		[DllImport("libspeexdsp", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void speex_preprocess_estimate_update(IntPtr st, Int16* x);
+		[DllImport("libspeexdsp", CallingConvention = CallingConvention.Cdecl)]
+		public static extern int speex_preprocess_ctl(IntPtr st, int request, void* ptr);
+	}
+}
